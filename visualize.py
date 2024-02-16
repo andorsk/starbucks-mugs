2c8f4efdd8353b83093274d9363496445728e2cc
Andor Kesselman <andor@magic-pro-3.local>
2024-02-15 18:47:27 -0800

    updated index and icon

--- visualize.py
+++ visualize.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 import json
 import folium
+import base64
+
+def modify_and_encode_svg(svg_path, new_color):
+    with open(svg_path, 'r') as file:
+        svg_content = file.read()
+    encoded_svg = svg_content
+    modified_svg_content = encoded_svg.replace('#000000', f'{new_color}')
+    svg_base64 =  base64.b64encode(modified_svg_content.encode('utf-8')).decode()
+    data_uri = f"data:image/svg+xml;base64,{svg_base64}"
+    return data_uri
 
 # Initialize a map centered around Charlotte
 f = open('final_data.json', 'r')
@@ -17,13 +27,15 @@ for c, d in data.items():
     description = f'<img src="{d["img"]}" width="200"><br><a href="{d["url"]}">Link</a><br>{d["description"]}'
     iframe = folium.IFrame(description, width=250, height=300)
     popup = folium.Popup(iframe, max_width=300)
-    c = 'green' if d['owned'] is True else 'yellow'
+    c = 'green' if d['owned'] is True else 'orange'
+    icon_image = modify_and_encode_svg('./icon.svg', c)
+    icon = folium.CustomIcon(icon_image, icon_size=(30, 30))  # Adjust size as needed
     folium.Marker(
         location=d["latlong"],
         popup=popup,
-        icon=folium.Icon(color=c),  # This sets the marker color to black
+        icon=icon,
         fill_opacity=0.6,
         tooltip=tooltip
     ).add_to(m)
 
-m.save('map.html')
+m.save('index.html')
